31897bccc00825068e8abe1278983d5d18722c66
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2021-07-18 16:20:08 +0200

    enable services

--- examples/using_defaults/main.tf
+++ examples/using_defaults/main.tf
@@ -3,11 +3,21 @@ provider "google-beta" {}
 
 
 module "privatebin" {
-  source  = "git::https:/github.com/binxio/terraform-google-privatebin.git?ref=0.1.0"
+  source = "git::https:/github.com/binxio/terraform-google-privatebin.git?ref=0.1.0"
 
   providers = {
     google      = google
     google-beta = google-beta
   }
+  depends_on = [google_project_service.run, google_project_service.secretmanager]
 }
 
+resource "google_project_service" "run" {
+  service            = "run.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "secretmanager" {
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
+}
